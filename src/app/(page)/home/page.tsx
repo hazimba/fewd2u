4ca076503a7dcebba995e5db2b4957499f307820
Hazim Bakar <hazimbaa@gmail.com>
2025-09-27 18:22:47 +0800

    applied force-dynamic

--- src/app/(page)/home/page.tsx
+++ src/app/(page)/home/page.tsx
@@ -1,8 +1,16 @@
 import { User } from "@/types";
+import { baseUrl } from "@/lib/getBaseUrl";
+
+export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const res = await fetch(`${process.env.APP_URL}/api/users`);
+  const res = await fetch(`${baseUrl}/api/users`, { cache: "no-store" });
+  if (!res.ok) {
+    throw new Error("Failed to fetch users");
+  }
   const users: User[] = await res.json();
+
+  console.log("Users (Server Component):", users);
 
   return (
     <div>
