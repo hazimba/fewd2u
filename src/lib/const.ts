7022b3f14d41d2e28ddb94c15f3bcf14047309ef
Hazim Bakar <hazimbaa@gmail.com>
2025-09-28 00:36:25 +0800

    coming soon page added

---
+++ src/lib/const.ts
@@ -0,0 +1,5 @@
+export const NAVIGATION_LINKS = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  //   { name: "Contact", href: "/contact" },
+];
--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,2 +1,3 @@
+// NEXT_PUBLIC_BASE_URL will be https://fewd2u.vercel.app default setting by Vercel, localhost is there for local dev
 export const baseUrl =
   process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
