b79b8af3c78f733398c70befa37079f62c4f3bd2
Hazim Bakar <hazimbaa@gmail.com>
2025-10-04 16:34:20 +0800

    contact us page

--- src/app/(page)/home/page.tsx
+++ src/app/(page)/home/page.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import FeatureSection from "./Feature";
 import OurProduct from "./OurProduct";
 import ServiceSection from "./ServiceSection";
+import { Contact } from "lucide-react";
+import ContactUsSection from "./ContactUsSection";
 
 export const dynamic = "force-dynamic";
 
@@ -13,23 +15,7 @@ export default async function HomePage() {
       <FeatureSection />
       <OurProduct />
       <ServiceSection />
-      <div className="relative">
-        <div className="w-full text-center lg:h-[40vh] h-[60vh] py-10">
-          <Image
-            src="/products/wallpaper-product-2.jpg"
-            alt="Contact Us"
-            fill
-            className=" opacity-10 pt-48 lg:pt-0 object-cover object-center"
-          />
-          <div className="relative z-20 lg:top-20 top-50">
-            <h2 className="text-2xl z-20 top-0 font-bold">Contact Us!</h2>
-            <p className="text-gray-600 text-sm px-4 mt-2 max-w-lg mx-auto">
-              From classic recipes to modern favorites, taste the love and
-              effort in every bite prepared in our kitchen.
-            </p>
-          </div>
-        </div>
-      </div>
+      <ContactUsSection />
     </div>
   );
 }
